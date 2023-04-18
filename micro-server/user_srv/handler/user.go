afc6e4c85aa94b7f748016d6a71d4c710497eba5
yul <1103221645@qq.com>
2023-04-18 21:38:15 +0800

    用户列表文件

---
+++ micro-server/user_srv/handler/user.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+	"context"
+	"gorm.io/gorm"
+	"micro-server/user_srv/global"
+	"micro-server/user_srv/model"
+	"micro-server/user_srv/proto"
+)
+
+func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page == 0 {
+			page = 1
+		}
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
+
+func Model2Response(user model.User) proto.UserInfoResponse {
+	// 在grpc的message中，字段有默认值，不能随意赋值nil
+	//
+	userInfoRes := proto.UserInfoResponse{
+		Id:       user.ID,
+		Password: user.Password,
+		NickName: user.NickName,
+		Gender:   user.Gender,
+		Role:     int32(user.Role),
+	}
+	if user.Birthday != nil {
+		userInfoRes.BirthDay = unit64(user.Birthday.Unix())
+	}
+	return userInfoRes
+}
+
+type UserServer struct{}
+
+// GetUserList 获取用户列表
+func (s *UserServer) GetUserList(ctx context.Context, req proto.PageInfo) (*proto.UserListResponse, error) {
+	var users []model.User
+	result := global.DB.Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	rsp := &proto.UserListResponse{}
+	rsp.Total = int32(result.RowsAffected)
+
+	global.DB.Scopes(Paginate(int(req.Pn), int(req.PSize))).Find(&users)
+
+	for _, user := range users {
+		userInfoRsp := Model2Response(user)
+		rsp.Data = append(rsp.Data, &userInfoRsp) // 这里&userInfoRsp需要注意，要先赋值，再使用
+	}
+
+	return rsp, nil
+
+}
--- micro-server/user_srv/model/main/main.go
+++ micro-server/user_srv/model/main/main.go
@@ -20,32 +20,7 @@ func genMd5(code string) string {
 }
 
 func main() {
-	//// 连接数据库
-	//dsn := "root:root@tcp(47.106.214.127:3306)/mxshop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
-	//
-	//// 日志
-	//newLogger := logger.New(
-	//	log.New(os.Stdout, "\r\n", log.LstdFlags),
-	//	logger.Config{
-	//		SlowThreshold: time.Second,
-	//		LogLevel:      logger.Info,
-	//		Colorful:      true,
-	//		//IgnoreRecordNotFoundError: true,
-	//		//ParameterizedQueries:      true,
-	//	},
-	//)
-	//
-	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-	//	Logger: newLogger,
-	//	NamingStrategy: schema.NamingStrategy{
-	//		TablePrefix:   "",   // 自动加表名前缀
-	//		SingularTable: true, // 表名单数形式
-	//		NameReplacer:  nil,
-	//	},
-	//})
-	//if err != nil {
-	//	panic(err)
-	//}
+
 	//
 	//_ = db.AutoMigrate(&model.User{})
 
