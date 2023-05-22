a9a3135f842def7bd6b4245a9bc6d13a9798b8ca
yul <1103221645@qq.com>
2023-05-22 20:53:16 +0800

    custom mobile phone validate rules

--- go-api/user-web/main.go
+++ go-api/user-web/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin/binding"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
 	"go-api/user-web/global"
 	"go-api/user-web/initialize"
+	myValidator "go-api/user-web/validator"
 	"go.uber.org/zap"
 )
 
@@ -20,6 +24,17 @@ func main() {
 	// 初始化翻译
 	if err := initialize.InitTrans("zh"); err != nil {
 		panic(err)
+	}
+
+	// 注册验证器
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("mobile", myValidator.ValidateMobile)
+		_ = v.RegisterTranslation("mobile", global.Trans, func(ut ut.Translator) error {
+			return ut.Add("mobile", "{0} 非法的手机号码", true)
+		}, func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("mobile", fe.Field())
+			return t
+		})
 	}
 
 	zap.S().Infof("启动服务器，端口：%d", global.ServerConfig.Port)
---
+++ go-api/user-web/validator/validators.go
@@ -0,0 +1,15 @@
+package validator
+
+import (
+	"github.com/go-playground/validator/v10"
+	"regexp"
+)
+
+func ValidateMobile(fl validator.FieldLevel) bool {
+	mobile := fl.Field().String()
+	ok, _ := regexp.MatchString("/^1(3\\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\\d|9[0-35-9])\\d{8}$/", mobile)
+	if !ok {
+		return false
+	}
+	return true
+}
