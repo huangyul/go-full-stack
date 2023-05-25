de5257221461de1066c2572217fb9d8a3b16e5d6
yul <1103221645@qq.com>
2023-05-26 06:45:08 +0800

    cross-domain middleware

--- go-api/user-web/initialize/router.go
+++ go-api/user-web/initialize/router.go
@@ -2,11 +2,15 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-api/user-web/middlewares"
 	router2 "go-api/user-web/router"
 )
 
 func Routers() *gin.Engine {
 	router := gin.Default()
+
+	// 配置跨域
+	router.Use(middlewares.Cors())
 
 	// 路由初始化
 	ApiGroup := router.Group("/v1")
