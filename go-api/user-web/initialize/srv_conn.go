4ce1558217ff75906063857d6de77371edee795f
yul <1103221645@qq.com>
2023-06-10 21:50:49 +0800

    load balancing

--- go-api/user-web/initialize/srv_conn.go
+++ go-api/user-web/initialize/srv_conn.go
@@ -3,6 +3,7 @@ package initialize
 import (
 	"fmt"
 	"github.com/hashicorp/consul/api"
+	_ "github.com/mbobakov/grpc-consul-resolver"
 	"go-api/user-web/global"
 	"go-api/user-web/proto"
 	"go.uber.org/zap"
@@ -10,6 +11,24 @@ import (
 )
 
 func InitSrvConn() {
+	consuleInfo := global.ServerConfig.ConsulInfo
+	userConn, err := grpc.Dial(
+		fmt.Sprintf("consul://%s:%d/%s?wait=14s", consuleInfo.Host, consuleInfo.Port, global.ServerConfig.UserSrvInfo.Name),
+		grpc.WithInsecure(),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+	)
+	if err != nil {
+		fmt.Println(err)
+
+		zap.S().Fatal("[InitSrvConn] 连接 【用户服务失败】")
+	}
+
+	// 调用接口
+	userSrvClient := proto.NewUserClient(userConn)
+	global.UserSrvClient = userSrvClient
+}
+
+func InitSrvConn2() {
 	// 从注册中心获取用户信息
 	consulConfig := api.DefaultConfig()
 	consulConfig.Address = fmt.Sprintf("%s:%d", global.ServerConfig.ConsulInfo.Host, global.ServerConfig.ConsulInfo.Port)
